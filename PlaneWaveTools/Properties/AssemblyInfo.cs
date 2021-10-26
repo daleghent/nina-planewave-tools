801950028219cc1ef30df61bc15c061daac5b88d
Dale Ghent <daleg@elemental.org>
2021-10-26 19:56:03 -0400

    Add logo url

--- PlaneWaveTools/Properties/AssemblyInfo.cs
+++ PlaneWaveTools/Properties/AssemblyInfo.cs
@@ -46,7 +46,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/daleghent/nina-plugins/blob/main/PlaneWaveTools/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
-[assembly: AssemblyMetadata("FeaturedImageURL", "")]
+[assembly: AssemblyMetadata("FeaturedImageURL", "https://daleghent.github.io/nina-plugins/assets/images/planewavetools.png")]
 //[Optional] A url to an example screenshot of your plugin in action
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 //[Optional] An additional url to an example example screenshot of your plugin in action
